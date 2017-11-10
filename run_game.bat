2a963c77827540a78dc205088c625f96eb42778f
Florian Witteler <florian_witteler@gmx.de>
2017-11-10 14:43:50 +0100

    initial commit

---
+++ run_game.bat
@@ -0,0 +1,2 @@
+sbt assembly
+\halite.exe -d "240 160" "java -jar target\scala-2.12\MyBot.jar" "java -jar target\scala-2.12\MyBot.jar"
---
+++ run_game.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sbt assembly
+./halite -d "240 160" "java -jar ./target/scala-2.12/MyBot.jar" "java -jar ./target/scala-2.12/MyBot.jar"
